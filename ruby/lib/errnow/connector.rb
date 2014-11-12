e9c489f9b7e734e412749d97b844fd07562c706c
mryoshio <yoshiokaas@gmail.com>
2014-11-13 00:44:36 +0900

    implement to get custom page

--- ruby/lib/errnow/connector.rb
+++ ruby/lib/errnow/connector.rb
@@ -1,6 +1,28 @@
+require 'net/http'
+
 module Errnow
   class Connector
     def initialize
+      @url = 'https://github.com/'
+    end
+
+    # Get target endpoint.
+    #
+    # @param app_id [String] application id of yours
+    # @param status [Fixnum] status for the page
+    # @return res [Net::HTTPResponse]
+    def get(app_id = nil, status = nil)
+      params = { app_id: app_id, status: status }
+      uri = URI(@url)
+      uri.query = URI.encode_www_form(params)
+      Net::HTTP.start(
+        uri.host,
+        uri.port,
+        use_ssl: uri.scheme == 'https') do |http|
+        req = Net::HTTP::Get.new(uri)
+        # req['Custom Header'] = 'foo'
+        res = http.request(req)
+      end
     end
   end
 end
--- ruby/lib/errnow/instance_methods.rb
+++ ruby/lib/errnow/instance_methods.rb
@@ -2,11 +2,17 @@ require_relative 'connector'
 
 module Errnow
   module InstanceMethods
+    # Render custom page in error.
+    #
     def render_in_err
       return if status == 200
-      # TODO: connect server and get html
-      # Errnow::Connector.new
-      response.body = "Error Status: #{status}"
+      connector = Errnow::Connector.new
+      r = connector.get('xxx', status) # sample
+      if r.content_type == 'text/html' && r.code_type == Net::HTTPOK
+        response.body = r.body
+      else
+        response.body = "Error Status: #{status}"
+      end
     end
   end
 end
