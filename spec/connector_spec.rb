8068b170eccef9111e10531c2371506371ba408a
mryoshio <yoshiokaas@gmail.com>
2014-12-02 00:50:26 +0900

    fix test error

---
+++ spec/connector_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Errnow do
+  describe 'Connector' do
+    let(:connector) { Errnow::Connector.new }
+
+    describe '#initialize' do
+      it 'some test'
+    end
+
+    describe '#get' do
+      it 'some test'
+    end
+  end
+end
